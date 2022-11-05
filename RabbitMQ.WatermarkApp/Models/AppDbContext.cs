9e198dc2ec64408b8297d7905a4ba6a03dda4d69
Fatih Şallı <mimsallifatih@gmail.com>
2022-11-05 19:48:14 +0300

    .

---
+++ RabbitMQ.WatermarkApp/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RabbitMQ.WatermarkApp.Models
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- RabbitMQ.WatermarkApp/Startup.cs
+++ RabbitMQ.WatermarkApp/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.WatermarkApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,13 @@ namespace RabbitMQ.WatermarkApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(databaseName: "productDb");
+            });
+
+
+
             services.AddControllersWithViews();
         }
 
